01d20823bea9d7b23f2dfb6281b4cd495ea7d1e8
wakoTheDev <wakopaul19@gmail.com>
2025-11-29 09:40:19 +0300

    Fix subjects not loding in the edit uestion filter

--- frontend/exam/src/components/EditorDashboard.js
+++ frontend/exam/src/components/EditorDashboard.js
@@ -2898,6 +2898,7 @@ useEffect(() => {
     useEffect(() => {
         if (activeTab === 'edit') {
             handleSearchQuestions(searchQuery);
+            fetchSubjects();
         }
     }, [activeTab]);
 
@@ -6562,7 +6563,7 @@ useEffect(() => {
                                             }}
                                             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none text-sm bg-white mb-8"
                                         >
-                                            <option value=""></option>
+                                            <option value="">All Subjects</option>
                                             {existingSubjects.map(s => (
                                                 <option key={s.id} value={s.name}>{s.name}</option>
                                             ))}
