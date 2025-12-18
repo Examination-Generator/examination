7d1b06f473e980a2bc0ecaecb0da8f7482344f15
wakoTheDev <wakopaul19@gmail.com>
2025-12-18 17:21:38 +0300

    fixed missing vlidtion function

--- django_backend/api/biology_paper2_generation.py
+++ django_backend/api/biology_paper2_generation.py
@@ -538,7 +538,7 @@ def generate_biology_paper2(request):
         data = json.loads(request.body)
         
         paper_id = data.get('paper_id')
-        selected_topic_ids = data.get('topic_ids', [])
+        selected_topic_ids = data.get('selected_topics', [])
         
         if not paper_id or not selected_topic_ids:
             return JsonResponse({
