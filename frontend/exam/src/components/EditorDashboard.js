ee829c3604de1e01c2e4a681cdfe7c0889a95950
wakoTheDev <wakopaul19@gmail.com>
2025-12-31 17:08:21 +0300

    fixing missing posittion of edit nswer symbol

--- frontend/exam/src/components/EditorDashboard.js
+++ frontend/exam/src/components/EditorDashboard.js
@@ -8234,7 +8234,7 @@ useEffect(() => {
                                             <button
                                                 type="button"
                                                 onClick={() => {
-                                                    setEditSymbolPickerTarget('question');
+                                                    setEditSymbolPickerTarget('answer');
                                                     setShowEditSymbolPicker(true);
                                                 }}
                                                 className="bg-indigo-500 hover:bg-indigo-600 text-white px-3 py-1.5 rounded-lg transition text-xs flex items-center gap-1.5"
