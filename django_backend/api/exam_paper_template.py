9578ec3582ffe460975d1ca048523ef51b451cb6
wakoTheDev <wakopaul19@gmail.com>
2026-06-12 19:05:28 +0300

    size updatre

--- django_backend/api/exam_paper_template.py
+++ django_backend/api/exam_paper_template.py
@@ -312,18 +312,17 @@ def generate_full_exam_html(coverpage_data, questions, paper_data=None, coverpag
             
             .question-flow.business-paper2{{
                 max-width: 210mm;
-                margin-left: auto;
-                margin-right: auto;
+                margin: 0 auto;
                 text-align: left !important;
-                font-size: 14pt !important;
+                font-size: 15pt !important;
             }}
             .question{{
                 background-color: blue;
-                font-size: 12pt !important;
+                font-size: 15pt !important;
             }}
             .question-part{{
                 width: 90% !important;
-                font-size: 12pt !important; 
+                font-size: 15pt !important; 
             }}
             
             .question-flow.physics-paper1 {{
