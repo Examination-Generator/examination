6bdb54cf4a13d22949cfc150b9335b68f86b3910
wakoTheDev <wakopaul19@gmail.com>
2026-06-12 19:24:11 +0300

    size updatre

--- django_backend/api/exam_paper_template.py
+++ django_backend/api/exam_paper_template.py
@@ -378,6 +378,15 @@ def generate_full_exam_html(coverpage_data, questions, paper_data=None, coverpag
                 font-size: 12pt !important;
                 width: 210mm;
             }}
+            .question-con{{
+                margin: 0 auto;
+                box-shadow: none;
+                padding: 5mm !important;
+                min-height: 297mm;
+                page-break-after: always !important;
+                font-size: 12pt !important;
+                width: 210mm;
+            }}
             
             /* Ensure page number stays in footer */
             .page-number {{
@@ -976,7 +985,7 @@ def _generate_business_paper2_pages(questions, total_pages, coverpage_data=None)
             
             # Generate page HTML with combined question
             page_html = f"""
-    <div style="margin-top: auto; font-size:14pt; line-height:1.8;"  class="exam-page question-flow business-paper2">
+    <div style="margin-top: auto; font-size:14pt; line-height:1.8;"  class="exam-page question-con question-flow business-paper2">
         <div class="question">                
             <div class="question-part">
                 <div style="text-align: left;">
